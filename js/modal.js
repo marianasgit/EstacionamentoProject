35355d16363247864d81161f276134702e4898c7
MARIANA GOMES DOS SANTOS <you@example.com>
2022-06-03 15:08:48 -0300

    ajuste de modal de novo registro

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,28 @@
 'use strict'
 
-const openModal = () => document.getElementById('modal').classList.add('active')
+const abrirNovoRegistro = () => {
+    document.getElementById('modal').classList.add('active')
+}
 
-const closeModal = () => {
+const abrirFechamento = () => {
+    document.getElementById('fechamento').classList.add('active')
+}
+
+const fecharNovoRegistro = () => {
     document.getElementById('modal').classList.remove('active')
     document.getElementById('modal-form').reset()
 }
 
+const finalizarFechamento = () => {
+    document.getElementById('fechamento').classList.remove('active')
+}
 
+document.getElementById('btnAddVeiculo').addEventListener('click', abrirNovoRegistro)
 
-document.getElementById('modalClose').addEventListener('click', closeModal)
+document.getElementById('btnFecharRegistro').addEventListener('click', fecharNovoRegistro)
 
+document.getElementById('deletar').addEventListener('click', abrirFechamento)
+
+document.getElementById('buttonFinalizar').addEventListener('click', finalizarFechamento)
 
 document.getElementById('print').addEventListener('click', ()=>print())
